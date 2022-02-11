5658ad82006e3e3056e83a163c8440c6f905f28b
x10an14 <x10an14@users.noreply.github.com>
2022-02-11 14:22:04 +0100

    Feature: v1!

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
+// std lib
+use std::io::{stdin, BufRead};
 use std::process;
 
-use anyhow;
+// 3rd-party
+use clap::Parser;
 use simple_steam_totp;
 
-fn main() -> anyhow::Result<()> {
-    // Use example shared_secret given by simple_steam_totp library's README.md
-    let totp_code: String = match simple_steam_totp::generate("V59i4SUqNiuYDrssYyMz62RSI9k=") {
-        Ok(code) => code,
-        Err(e) => {
-            eprintln!("{}", e);
-            process::exit(1);
-        }
-    };
-    println!("Hello, world!\n\t{}", totp_code);
+
+#[derive(Debug, Parser)]
+/// A small utility meant to receive Steam Guard's base64 encoded "Shared Secret"(s),
+/// and print the corresponding Steam 2FA TOTP(s) -
+/// can receive inputs as variadics arguments or passed through stdin.
+///
+/// This tool is not intended to do anything else than translate Steam 2FA seeds -> Steam 2FA TOTP
+/// codes.
+///
+/// See below link for how to extract your Steam Guard's "Shared Secret":
+/// https://github.com/SteamTimeIdler/stidler/wiki/Getting-your-%27shared_secret%27-code-for-use-with-Auto-Restarter-on-Mobile-Authentication
+struct Cli {
+    /// One or more (space separated) Steam Guard's Base64 encoded Shared Secret(s)
+    /// (read: seed(s) for Steam 2FA TOTP)
+    shared_secrets: Vec<String>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Get args passed via cli
+    let cli = Cli::parse();
+    let mut shared_secrets = cli.shared_secrets;
+
+    // Read inputs from stdin
+    if !atty::is(atty::Stream::Stdin) {
+        shared_secrets.extend(stdin().lock().lines().map(|l| l.unwrap()).collect::<Vec<String>>())
+    }
+
+    if shared_secrets.is_empty() {
+        eprintln!("A base64 encoded string seed (or multiple) is required! Check usage through --help.");
+        process::exit(1);
+    }
+
+    for seed in shared_secrets {
+        let code = match simple_steam_totp::generate(&seed) {
+            Ok(code) => code,
+            Err(e) => panic!("{}", e),
+        };
+        println!("{}", code);
+    }
+
     Ok(())
 }
